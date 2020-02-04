6296792f87206f3bf7488c19bcf94f4f97fb46b0
matsuhaya <matsuhaya007@gmail.com>
2020-02-04 15:35:31 +0900

    sizeをwidth,heightに変更

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
 class Maze {
-  constructor(size) {
-    this.size = size;
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
     this.grid = []; // 壁の場合は1,道の場合は0を格納した二次元配列
     this.startCellList = []; // 壁を生成するスタート地点となるセルの候補を格納した二次元配列
   }
 
   makeGrid() {
-    for (let row = 0; row < this.size; row++) {
-      let _rowData = [];
-      for (let column = 0; column < this.size; column++) {
-        if (row === 0 || row === this.size - 1) {
-          _rowData.push(1);
+    for (let row = 0; row < this.height; row++) {
+      let cell = [];
+      for (let column = 0; column < this.width; column++) {
+        if (row === 0 || row === this.height - 1) {
+          cell.push(1);
         } else {
-          if (column === 0 || column === this.size - 1) {
-            _rowData.push(1);
+          if (column === 0 || column === this.width - 1) {
+            cell.push(1);
           } else {
-            _rowData.push(0);
+            cell.push(0);
           }
         }
       }
-      this.grid.push(_rowData);
+      this.grid.push(cell);
     }
   }
 
   // row,columnともに偶数となる座標を壁伸ばし開始座標(候補)としてリストアップ
   countStartCellList() {
-    for (let row = 1; row < this.size - 1; row++) {
-      for (let column = 1; column < this.size - 1; column++) {
+    for (let row = 1; row < this.height - 1; row++) {
+      for (let column = 1; column < this.width - 1; column++) {
         if (row % 2 === 0 && column % 2 === 0) {
           this.startCellList.push([row, column]);
         }
@@ -37,9 +38,9 @@ class Maze {
 
 // Mazeインスタンスのデータを元に、DOMを生成
 const drowMaze = maze => {
-  for (let row = 0; row < maze.size; row++) {
+  for (let row = 0; row < maze.height; row++) {
     let tr = $('<tr>');
-    for (let column = 0; column < maze.size; column++) {
+    for (let column = 0; column < maze.width; column++) {
       if (maze.grid[row][column]) {
         tr.append($('<td class="maze-cell -wall"></td>'));
       } else {
@@ -51,7 +52,8 @@ const drowMaze = maze => {
 };
 
 //サイズは必ず5以上の奇数で生成する
-const size = 7;
-const maze = new Maze(size);
+const width = 7;
+const height = 9;
+const maze = new Maze(width, height);
 maze.makeGrid();
 drowMaze(maze);
